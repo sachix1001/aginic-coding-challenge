f78cfc75c93403dab928b15b2345c81bbcf65b45
sachix1001 <sachix1001@gmail.com>
2020-08-03 11:18:18 +1000

    add job state display

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import ListGroup from "react-bootstrap/ListGroup";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const jobs = [{ id: 1, state: "progress" },{ id: 1000, state: "progress" }];
   return (
     <div className="container">
-      <Form>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Job URL</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
+      <div className="formContainer">
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Job URL</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
+      <br />
+      <Form.Label>Job state</Form.Label>
+      {jobs.map((job) => (
+        <JobStatus id={job.id} state={job.status} color="black"></JobStatus>
+      ))}
     </div>
+  );
+}
+
+function JobStatus(props) {
+  const { id, state, color, progress } = props;
+  return (
+    <ListGroup >
+      <ListGroup.Item>
+        <span className="hk-label">Job URL:</span> {id}
+      </ListGroup.Item>
+      <ListGroup.Item>
+        <span className="hk-label">State:</span> {state}
+      </ListGroup.Item>
+    </ListGroup>
   );
 }
 
